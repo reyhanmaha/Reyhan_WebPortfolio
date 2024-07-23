ba4174e1824a15732e9d308c4101063007cbad54
reyhanmaha <reyhan12maharaj@gmail.com>
2024-07-23 14:37:53 -0400

    changed app into an express app

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 
 const port=3000;
 const app=express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.get("/home",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/about",(req,res)=>{
+    res.render("about");
+});
+
+app.get("/contact",(req,res)=>{
+    res.render("contact");
+});
+
+app.get("/resume",(req,res)=>{
+    res.render("resume");
+});
 
 app.get("/blogHomePage",(req,res)=>{
-    res.render("blogHomePage.ejs");
+    res.render("blogHomePage");
 });
 
-app.get("/blogAddPost",(req,res)=>{
-    res.render("blogAddPost.ejs");
-});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });

c98b1851bd1b6f09eb18147dd47a2fb4d013aafb
reyhanmaha <reyhan12maharaj@gmail.com>
2024-09-30 09:51:12 -0400

    removed pg

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from "path";
 import axios from "axios";
 import pg from "pg";
 import { info } from "console";
-
+/*
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -14,6 +14,7 @@ const db = new pg.Client({
 });
 
 db.connect();
+*/
 const drinksData=[
     {
         id:1,
